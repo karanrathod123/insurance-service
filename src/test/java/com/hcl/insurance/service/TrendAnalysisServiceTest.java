8250c4975209124bf54dae459aedfef65d64bc8b
prajaktajoshi123 <joshiprajakta12493@gmail.com>
2019-08-01 19:56:07 +0530

    trend test of simar

--- src/test/java/com/hcl/insurance/service/TrendAnalysisServiceTest.java
+++ src/test/java/com/hcl/insurance/service/TrendAnalysisServiceTest.java
@@ -1,5 +1,13 @@
 package com.hcl.insurance.service;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -8,6 +16,7 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import com.hcl.insurance.dto.CalculatedPolicy;
 import com.hcl.insurance.dto.ResponseData;
 import com.hcl.insurance.repository.OptedPoliciesRepository;
 
@@ -22,6 +31,20 @@ public class TrendAnalysisServiceTest {
 	
 	@Before
 	public void setUp() throws Exception {
+		
+		List<CalculatedPolicy> calculatedPolicyList=new ArrayList<>();
+		
+		CalculatedPolicy calculatedPolicy=new CalculatedPolicy();
+		calculatedPolicy.setMonth(1);
+		calculatedPolicy.setPercentage(10.15);
+		calculatedPolicy.setPolicyCount(2);
+		calculatedPolicy.setPolicyId(201l);
+		calculatedPolicy.setPolicyName("Life Insurance");
+		calculatedPolicy.setWeek(3);
+		
+		
+		
+		
 	}
 
 	@Test
@@ -30,8 +53,38 @@ public class TrendAnalysisServiceTest {
 		Mockito.when(optedPoliciesRepository.findBySearchTermTotalPolicies()).thenReturn(3);
 		
 		ResponseData actualResponse = trendAnalysisService.getPoliciesTrends("weekly");
-		//assertEquals(expectedResponse, actualResponse.getData());
-		//assertTrue(true);
+		assertEquals(expectedResponse, actualResponse.getData());
+		assertTrue(true);
 	}
 
+	
+	@Test
+	public void testGetPoliciesTrends1() {
+		
+		Mockito.when(optedPoliciesRepository.findBySearchTermTotalPolicies()).thenReturn(2);
+		
+		ResponseData actualResponse = trendAnalysisService.getPoliciesTrends("monthly");
+		assertEquals(expectedResponse, actualResponse.getData());
+		assertTrue(true);
+	}
+	
+	
+	@Test
+	public void testGetPoliciesTrends2() {
+		
+		Mockito.when(optedPoliciesRepository.findBySearchTermTotalPolicies()).thenReturn(1);
+		
+		ResponseData actualResponse = trendAnalysisService.getPoliciesTrends("all");
+		assertEquals(expectedResponse, actualResponse.getData());
+		assertTrue(true);
+	}
+	
+	@Test
+	public void getAllProductTestWhenEmpty() {
+
+		Mockito.when(optedPoliciesRepository.findBySearchTermTotalPolicies()).thenReturn(null);
+		ResponseData response = trendAnalysisService.getPoliciesTrends("all");
+		assertNotNull(response);
+	}
+	
 }
