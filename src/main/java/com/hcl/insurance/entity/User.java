e4608f05f2b19a702da01baeea459fb83f440d39
amoljadhav531 <amoljadhav531@gmail.com>
2019-08-01 12:42:24 +0530

    Added mapping

--- src/main/java/com/hcl/insurance/entity/User.java
+++ src/main/java/com/hcl/insurance/entity/User.java
@@ -1,9 +1,13 @@
 package com.hcl.insurance.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 import lombok.Data;
 
@@ -14,7 +18,13 @@ public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long userId;
+
 	private String userName;
+
 	private String mobileNumber;
+
 	private String email;
+
+	@OneToMany(mappedBy = "userId")
+	List<OptedPolicies> userPolicies = new ArrayList<>();
 }
