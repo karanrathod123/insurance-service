656fb9a418b275b7c9c5ec7aaaabbabee52eaa6d
karanrathod123 <karanrathod2256@gmail.com>
2019-08-01 19:27:29 +0530

    Chnages created as List Removed

--- src/main/java/com/hcl/insurance/service/PolicyDetailsServiceImpl.java
+++ src/main/java/com/hcl/insurance/service/PolicyDetailsServiceImpl.java
@@ -1,14 +1,11 @@
 package com.hcl.insurance.service;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
-import com.hcl.insurance.dto.PolicyDetailsDto;
 import com.hcl.insurance.dto.ResponseData;
 import com.hcl.insurance.entity.Policy;
 import com.hcl.insurance.repository.PolicyRepository;
@@ -18,28 +15,19 @@ public class PolicyDetailsServiceImpl implements PolicyDetailsService {
 
 	@Autowired
 	private PolicyRepository policyRepository;
-	
-	
-	
-	
+
 	@Override
 	public ResponseData detailsOfPolicy(Long id) {
-		PolicyDetailsDto policyDetailsDto;
-		
+
 		Optional<Policy> policyOptional = policyRepository.findById(id);
 		ResponseData responseData = new ResponseData();
-		if(policyOptional.isPresent()) {
-			Policy policy=policyOptional.get();
-			policyDetailsDto = new PolicyDetailsDto();
-			policyDetailsDto.setPolicyId(policy.getPolicyId());
-			policyDetailsDto.setPolicyName(policy.getPolicyName());
-			policyDetailsDto.setSumAssured(policy.getSumAssured());
-			policyDetailsDto.setTerm(policy.getPolicyTerm());
-			responseData.setData(policyDetailsDto);
+		if (policyOptional.isPresent()) {
+			Policy policy = policyOptional.get();
+			responseData.setData(policy);
 			responseData.setHttpStatus(HttpStatus.OK);
 			responseData.setMessage("Details of Policy Are as Follows");
-			
-		}else {
+
+		} else {
 			responseData.setHttpStatus(HttpStatus.BAD_REQUEST);
 			responseData.setMessage("Details Are Not Present For Following User ");
 		}
