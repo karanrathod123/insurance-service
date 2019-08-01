ce88ddc788276ff89079ff5b88febef9b0305369
Harsimar <simarkaur1295@gmail.com>
2019-08-01 18:35:16 +0530

    Add sonar changes

--- src/main/java/com/hcl/insurance/service/TrendAnalysisService.java
+++ src/main/java/com/hcl/insurance/service/TrendAnalysisService.java
@@ -1,7 +1,6 @@
 package com.hcl.insurance.service;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
---
+++ src/test/java/com/hcl/insurance/service/TrendAnalysisServiceTest.java
@@ -0,0 +1,39 @@
+package com.hcl.insurance.service;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import com.hcl.insurance.dto.ResponseData;
+import com.hcl.insurance.repository.OptedPoliciesRepository;
+
+@RunWith(MockitoJUnitRunner.class)
+public class TrendAnalysisServiceTest {
+
+	ResponseData expectedResponse;
+	@Mock
+	OptedPoliciesRepository optedPoliciesRepository;
+	@InjectMocks
+	TrendAnalysisService trendAnalysisService;
+	
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testGetPoliciesTrends() {
+		
+		Mockito.when(optedPoliciesRepository.findBySearchTermTotalPolicies()).thenReturn(3);
+		
+		ResponseData actualResponse = trendAnalysisService.getPoliciesTrends("weekly");
+		assertEquals(expectedResponse, actualResponse.getData());
+		assertTrue(true);
+	}
+
+}
